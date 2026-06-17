62f51eb93b8bef2d526559c153192f4486a7da02
enoch123code <Enoch.ajewole@gmail.com>
2026-06-17 14:28:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Quantum Computing Project Loaded");
+console.log("Quantum Computing Simulator Loaded");
+
+window.onload = function () {
+    console.log("Website Ready");
+
+    alert(
+        "Welcome to the Quantum Computing Simulator!\n\n" +
+        "This project demonstrates the Deutsch-Jozsa and Bernstein-Vazirani algorithms."
+    );
+};
